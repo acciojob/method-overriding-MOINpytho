3b78ccb65e92580762460442402a458be101df03
Moin <53350043+MOINpytho@users.noreply.github.com>
2024-11-04 20:40:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+   public static class A{
+      public String meth() {
+          return "Invoking method from class A";
+      }
+  }
+  public static  class  B extends A{
+      public String meth() {
+          return "Invoking method from class A";
+      }
+  }
+
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.print(obj.meth());
+    }
 }
